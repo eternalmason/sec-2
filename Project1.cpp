1f90555ff336437e357ac6e579b0cfe141b5b546
eternalmason <147970382+eternalmason@users.noreply.github.com>
2023-10-15 10:33:17 +0300

    Add files via upload

---
+++ Project1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, sum;
+    cout << "enter two numbers" << endl;
+    cin >> x >> y;
+    sum = x + y;
+    cout << "The sum of numbers =" << " " << sum;
+    return 0;
+}
